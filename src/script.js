ff528e0fe847f1932fc0649ab4e7b946a32be4b5
ayuarista <aristaadewi8@gmail.com>
2025-01-31 14:28:16 +0800

    create services

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function ourService(){
+    return{
+        item: [
+            {
+                id: 1,
+                title: "Social Media Management",
+                name: "shapes-outline",
+                description: "We manage your social media presence to engage your audience and grow your brand.",
+            }
+        ]
+    }
+}
