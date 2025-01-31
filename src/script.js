bbe4b377d4b3eb75269ff63781dc59600c53ad15
ayuarista <aristaadewi8@gmail.com>
2025-01-31 14:30:20 +0800

    add data

--- src/script.js
+++ src/script.js
@@ -4,8 +4,8 @@ function ourService(){
             {
                 id: 1,
                 title: "Social Media Management",
-                name: "shapes-outline",
-                description: "We manage your social media presence to engage your audience and grow your brand.",
+                icons: "shapes-outline",
+                desc: "We manage your social media presence to engage your audience and grow your brand.",
             }
         ]
     }
