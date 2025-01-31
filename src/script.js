04d5017943951bb880c7efa93c2248a4457d973d
ayuarista <aristaadewi8@gmail.com>
2025-01-31 14:45:44 +0800

    create why us

--- src/script.js
+++ src/script.js
@@ -1,12 +1,30 @@
-function ourService(){
-    return{
-        item: [
-            {
-                id: 1,
-                title: "Social Media Management",
-                icons: "shapes-outline",
-                desc: "We manage your social media presence to engage your audience and grow your brand.",
-            }
-        ]
-    }
+function ourService() {
+  return {
+    items: [
+      {
+        id: 1,
+        title: "Graphic Design",
+        icons: "color-palette-outline",
+        desc: "We create stunning visuals that capture your brand's essence and communicate your message effectively.",
+      },
+      {
+        id: 2,
+        title: "Web Design",
+        icons: "code-slash-outline",
+        desc: "We design responsive and user-friendly websites that reflect your brand and engage your audience.",
+      },
+      {
+        id: 3,
+        title: "Branding",
+        icons: "shapes-outline",
+        desc: "We help you build a strong brand identity with cohesive designs that resonate with your audience.",
+      },
+      {
+        id: 4,
+        title: "UI/UX Design",
+        icons: "phone-portrait-outline",
+        desc: "We craft intuitive and visually appealing interfaces to enhance user experience and satisfaction.",
+      },
+    ],
+  };
 }
