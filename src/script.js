e74ac5e7f88f422b03a5b136b717a0162c8537de
ayuarista <aristaadewi8@gmail.com>
2025-02-01 09:46:25 +0800

    create footer

--- src/script.js
+++ src/script.js
@@ -28,3 +28,66 @@ function ourService() {
     ],
   };
 }
+
+function ourPortofolio(){
+    return{
+        items:[
+            {
+                id: 1,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img: "https://i.pinimg.com/736x/19/09/8b/19098b48a9f4a589ec702643ccf528ae.jpg",
+                link: "https://i.pinimg.com/736x/19/09/8b/19098b48a9f4a589ec702643ccf528ae.jpg"
+            },
+            {
+                id: 2,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img: "https://i.pinimg.com/736x/2b/51/10/2b51108290f5d00fdb1be01faf772474.jpg",
+                link: "https://i.pinimg.com/736x/2b/51/10/2b51108290f5d00fdb1be01faf772474.jpg",
+            },
+            {
+                id: 3,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img: "https://i.pinimg.com/736x/a6/86/6e/a6866ed3a1e3e784e37446f844b9423d.jpg",
+                link: "https://i.pinimg.com/736x/a6/86/6e/a6866ed3a1e3e784e37446f844b9423d.jpg",
+            },
+            {
+                id:4,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img:"https://i.pinimg.com/736x/ec/26/81/ec2681706f8150a8402d84b39d7a3938.jpg",
+                link:"https://i.pinimg.com/736x/ec/26/81/ec2681706f8150a8402d84b39d7a3938.jpg"
+            },
+            {
+                id:5,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img:"https://i.pinimg.com/736x/66/a3/a2/66a3a2de5b06980121e00fabb262ddc3.jpg",
+                link:"https://i.pinimg.com/736x/66/a3/a2/66a3a2de5b06980121e00fabb262ddc3.jpg"
+            },
+            {
+                id:6,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img:"https://i.pinimg.com/736x/1e/9f/b1/1e9fb1830106840a9d06269cee7b2575.jpg",
+                link:"https://i.pinimg.com/736x/1e/9f/b1/1e9fb1830106840a9d06269cee7b2575.jpg"
+            },
+            {
+                id: 7,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img: "https://i.pinimg.com/736x/f8/d9/c1/f8d9c17c7db58d869a80268e23ca2cd1.jpg",
+                link: "https://i.pinimg.com/736x/f8/d9/c1/f8d9c17c7db58d869a80268e23ca2cd1.jpg",
+            },
+            {
+                id: 8,
+                title:"Cr: Pinterest",
+                desc:"Not mine",
+                img: "https://i.pinimg.com/736x/9d/d5/59/9dd559140fe2a204f957b7f3d7f3396e.jpg",
+                link: "https://i.pinimg.com/736x/9d/d5/59/9dd559140fe2a204f957b7f3d7f3396e.jpg",
+            }
+        ]
+    }
+}
